0b7038246fb850cdf65e9884689d8af92b8fc462
Matheus Gomes <math.s.gomes@gmail.com>
2025-03-23 22:20:22 -0300

    add result win or lose

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ new Vue({
         playerLife: 30,
         monsterLife: 19,
     },
-    computed: {},
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0;
+        }
+    },
     methods: {},
     watch: {}
 });
