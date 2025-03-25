d5f032d2a529b31ae110fb515459c236e3935684
Matheus Gomes <math.s.gomes@gmail.com>
2025-03-24 22:06:32 -0300

    add style logs

--- app.js
+++ app.js
@@ -16,26 +16,28 @@ new Vue({
             this.running = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+            this.logs = [];
         },
         attack(especial) {
             this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player');
             if (this.monsterLife > 0) {
-                this.hurt('playerLife', 7, 12, false, 'Monstro', 'monster');
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
             }
         },
         hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0;
             const hurt = this.getRandom(min + plus, max + plus);
             this[prop] = Math.max(this[prop] - hurt, 0);
-            this.registerLog(`${source} atingiu o ${target} com ${hurt}`, cls);
+            this.registerLog(`${source} atingiu o ${target} com ${hurt}.`, cls);
         },
         healAndHurt() {
             this.heal(10, 15);
-            this.hurt('playerLife', 7, 12, false);
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
         },
         heal(min, max) {
             const heal = this.getRandom(min, max);
             this.playerLife = Math.min(this.playerLife + heal, 100);
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player');
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min;
